45ee3daa5c8323d81b5378851d6991b458a4f04a
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-02-19 19:31:48 +0200

    Create login using flask-login

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask, render_template, redirect, url_for
+from flask_login import LoginManager, login_user, current_user
 from passlib.hash import pbkdf2_sha256
 from flask_socketio import SocketIO, emit
 from wtform_fields import *
@@ -13,7 +14,16 @@ socketio = SocketIO(app)
 
 # configure database
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://empawzdedkzmzu:ec111b26dda56b50cd61a648b60eebda842e2189b782ff308a8d56d3c1a512c6@ec2-184-72-236-3.compute-1.amazonaws.com:5432/d8s47squ17rp4l"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+# Configure flask login
+login = LoginManager(app)
+login.init_app(app)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -44,7 +54,11 @@ def login():
 
     # Allow login if validation success
     if login_form.validate_on_submit():
-        return "Logged in!"
+        user_object = User.query.filter_by(username=login_form.username.data).first()
+        login_user(user_object)
+        if current_user.is_authenticated:
+            return "Logged in!"
+        return "not logged in"
 
     return render_template('login.html', form=login_form)
 
