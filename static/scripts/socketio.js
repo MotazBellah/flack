85ca4a1fac59f4a7c6a540cd4cb9cfa1e8951e60
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-02-20 17:03:48 +0200

    Add socket io in client-side and build a custom socket io to connect and recieve the message from the server

---
+++ static/scripts/socketio.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Connect to websocket
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+    socket.on('connect', () => {
+        socket.send("Iam connected");
+    });
+
+    socket.on('message', data => {
+        console.log(`Message recived: ${data}`);
+    });
+
+
+
+})
