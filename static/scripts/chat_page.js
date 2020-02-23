92eb59939866ed5289d4d8a9dcb0e8b69047df93
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-02-23 03:56:21 +0200

    reset chat html page

--- static/scripts/chat_page.js
+++ static/scripts/chat_page.js
@@ -1,10 +1,4 @@
 document.addEventListener("DOMContentLoaded", () =>{
-
-    // Make sidebar collapse on click
-    // Room selection
-    document.querySelector('#show-sidebar-button').onclick = () => {
-        document.querySelector('#sidebar').classList.toggle('view-sidebar');
-    };
     // Make 'enter' key submit message
     let msg = document.querySelector('#user_message');
     msg.addEventListener('keyup', event => {
