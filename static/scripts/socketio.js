fe209e39763212616a72ba78ef55e04f6d160f91
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-02-23 03:48:45 +0200

    reset chat html page

--- static/scripts/socketio.js
+++ static/scripts/socketio.js
@@ -1,86 +1,54 @@
-document.addEventListener('DOMContentLoaded', () => {
-
+document.addEventListener("DOMContentLoaded", () => {
     // Connect to websocket
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
-    // Retrieve username
-    const username = document.querySelector('#get-username').innerHTML;
+    // make lounge is a default room
+    //  and add the user to it
+    let room = 'Lounge';
+    joinRoom("Lounge")
 
-    // Set default room
-    let room = "Lounge"
-    joinRoom("Lounge");
 
-    // Send messages
+    // recieve message from the server
+    // Create a pragraph contains username, data and time
+    socket.on('message', data => {
+        const p = document.createElement('p');
+        const span_username = document.createElement('span');
+        const span_timestamp = document.createElement('span');
+        const br = document.createElement('br');
+
+        // If user write message in one of the room
+        if(data.username){
+            span_username.innerHTML = data.username;
+            span_timestamp.innerHTML = data.time_stamp;
+            p.innerHTML = span_username.outerHTML + br.outerHTML +
+                          data.msg + br.outerHTML + span_timestamp.outerHTML;
+            document.querySelector('#display-message-section').append(p);
+        }
+        // If user join/leave the room
+        // no need to display the username
+        else {
+            printSysMsg(data.msg);
+        }
+
+    });
+
+
+    // Get the text on the input field and send it to the server once the button is clicked
     document.querySelector('#send_message').onclick = () => {
-        alert('h')
         socket.send({'msg': document.querySelector('#user_message').value,
                      'username': username, 'room': room
                  });
-
+        // Clear input area
         document.querySelector('#user_message').value = '';
-    };
 
-    // Display all incoming messages
-    socket.on('message', data => {
+    }
 
-        // Display current message
-        if (data.msg) {
-            const p = document.createElement('p');
-            const span_username = document.createElement('span');
-            const span_timestamp = document.createElement('span');
-            const br = document.createElement('br')
-            // Display user's own message
-            if (data.username == username) {
-                    p.setAttribute("class", "my-msg");
-
-                    // Username
-                    span_username.setAttribute("class", "my-username");
-                    span_username.innerHTML = data.username;
-
-                    // Timestamp
-                    span_timestamp.setAttribute("class", "timestamp");
-                    span_timestamp.innerHTML = data.time_stamp;
-
-                    // HTML to append
-                    p.innerHTML += span_username.outerHTML + br.outerHTML + data.msg + br.outerHTML + span_timestamp.outerHTML
-
-                    //Append
-                    document.querySelector('#display-message-section').append(p);
-            }
-            // Display other users' messages
-            else if (typeof data.username !== 'undefined') {
-                p.setAttribute("class", "others-msg");
-
-                // Username
-                span_username.setAttribute("class", "other-username");
-                span_username.innerHTML = data.username;
-
-                // Timestamp
-                span_timestamp.setAttribute("class", "timestamp");
-                span_timestamp.innerHTML = data.time_stamp;
-
-                // HTML to append
-                p.innerHTML += span_username.outerHTML + br.outerHTML + data.msg + br.outerHTML + span_timestamp.outerHTML;
-
-                //Append
-                document.querySelector('#display-message-section').append(p);
-            }
-            // Display system message
-            else {
-                printSysMsg(data.msg);
-            }
-
-
-        }
-        scrollDownChatWindow();
-    });
-
-    // Select a room
-    document.querySelectorAll('.select-room').forEach(p => {
+    // Room selection
+    const rooms = document.querySelectorAll('.select-room');
+    rooms.forEach((p) => {
         p.onclick = () => {
-            let newRoom = p.innerHTML
-            // Check if user already in the room
-            if (newRoom === room) {
+            let newRoom = p.innerHTML;
+            if (newRoom == room){
                 msg = `You are already in ${room} room.`;
                 printSysMsg(msg);
             } else {
@@ -88,55 +56,34 @@ document.addEventListener('DOMContentLoaded', () => {
                 joinRoom(newRoom);
                 room = newRoom;
             }
-        };
+
+        }
     });
 
-    // Logout from chat
-    document.querySelector("#logout-btn").onclick = () => {
-        leaveRoom(room);
-    };
-
-    // Trigger 'leave' event if user was previously on a room
+    // Leave room
     function leaveRoom(room) {
+        // use emit, beacuse its custom event
         socket.emit('leave', {'username': username, 'room': room});
-
-        document.querySelectorAll('.select-room').forEach(p => {
-            p.style.color = "black";
-        });
     }
 
-    // Trigger 'join' event
+    // Join room
     function joinRoom(room) {
-
-        // Join room
+        // use emit, beacuse its custom event
         socket.emit('join', {'username': username, 'room': room});
-
-        // Highlight selected room
-        document.querySelector('#' + CSS.escape(room)).style.color = "#ffc107";
-        document.querySelector('#' + CSS.escape(room)).style.backgroundColor = "white";
-
-        // Clear message area
-        document.querySelector('#display-message-section').innerHTML = '';
-
+        // clear message area
+        document.querySelector("#display-message-section").innerHTML = '';
         // Autofocus on text box
         document.querySelector("#user_message").focus();
-    }
-
-    // Scroll chat window down
-    function scrollDownChatWindow() {
-        const chatWindow = document.querySelector("#display-message-section");
-        chatWindow.scrollTop = chatWindow.scrollHeight;
     }
 
     // Print system messages
     function printSysMsg(msg) {
         const p = document.createElement('p');
-        p.setAttribute("class", "system-msg");
         p.innerHTML = msg;
-        document.querySelector('#display-message-section').append(p);
-        scrollDownChatWindow()
-
-        // Autofocus on text box
+        document.querySelector("#display-message-section").append(p)
         document.querySelector("#user_message").focus();
     }
-});
+
+
+
+})
