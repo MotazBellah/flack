89bd22a420932563f2b342c05afb09d08450e19a
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-06-24 15:24:51 +0200

    Use gloable variable version for the app

--- application.py
+++ application.py
@@ -1,144 +1,134 @@
 import os
 from time import localtime, strftime
-from flask import Flask, render_template, redirect, url_for, flash, request, session
-from flask_login import LoginManager, login_user, current_user, login_required, logout_user
-from passlib.hash import pbkdf2_sha256
+from flask import Flask, render_template, redirect, url_for, flash, request, session, jsonify
 from flask_socketio import SocketIO, emit, send, join_room, leave_room
-from wtform_fields import *
-from models import *
+from werkzeug.utils import secure_filename
+from flask import send_from_directory
 
-# configure app
+
 app = Flask(__name__)
-app.config["SECRET_KEY"] = 'Super_Secret'
-app.config['WTF_CSRF_SECRET_KEY'] = "b'f\xfa\x8b{X\x8b\x9eM\x83l\x19\xad\x84\x08\xaa"
-#  Initialize Flask-socketIO
+app.config["SECRET_KEY"] = 'Super_secret_key'
 socketio = SocketIO(app)
 #  Create predefined rooms
-ROOMS = ['lounge', 'news', 'games', 'coding']
+ROOMS = []
+mesage = {}
 
-# configure database
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///chat.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-# Configure flask login
-login = LoginManager(app)
-login.init_app(app)
+UPLOAD_FOLDER = './static/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# app.config['WTF_CSRF_SECRET_KEY'] = "b'f\xfa\x8b{X\x8b\x9eM\x83l\x19\xad\x84\x08\xaa"
 
 
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload-file', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(url_for('index'))
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(url_for('index'))
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return jsonify({'filename': filename})
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    reg_form = RegistrationForm()
-    if reg_form.validate_on_submit():
-        username = reg_form.username.data
-        password = reg_form.password.data
-        # hash the password, and save it in db
-        hashed_pswd = pbkdf2_sha256.hash(password)
-
-        # Add user to DB
-        user = User(username=username, password=hashed_pswd)
-        db.session.add(user)
-        db.session.commit()
-
-        flash("Registered succesfully. Please login.", 'success')
+    login = 'loggedout'
+    username = ''
+    if 'username' in session:
+        login = 'loggedin'
+        username = session['username']
+    else:
         return redirect(url_for('login'))
 
-    return render_template('index.html', form=reg_form)
+    return render_template('chat.html', username=username, login=login, ROOMS=ROOMS)
 
-
-@app.route('/login', methods=['GET','POST'])
-def login():
-    login_form = LoginForm()
-
-    # Allow login if validation success
-    if login_form.validate_on_submit():
-        user_object = User.query.filter_by(username=login_form.username.data).first()
-        login_user(user_object)
-        return redirect(url_for('chat'))
-
-    return render_template('login.html', form=login_form)
-
-
-@app.route('/chat', methods=['GET', 'POST'])
-def chat():
-    if not current_user.is_authenticated:
-        flash("Please login", 'danger')
-        return redirect(url_for('login'))
-    rooms = Room.query.all()
-    print(rooms)
-    return render_template('chat.html', username=current_user.username, rooms=rooms)
-
-
-# @app.route('/create', methods=['POST'])
-# def create():
-#     if not current_user.is_authenticated:
-#         flash("Please login", 'danger')
-#         return redirect(url_for('login'))
-#
-#     room = request.form['room']
-#     print('/////////////////')
-#     print(room)
-#     print('/////////////////')
-#     if room in ROOMS:
-#         return redirect(url_for('chat'))
-#
-#     ROOMS.append(room)
-#     print(ROOMS)
-#     return redirect(url_for('chat', rooms=ROOMS))
 
 @app.route('/get-rooms', methods=['POST'])
 def get_rooms():
-    print('%%%%%%%%%%%%%%%%%')
-    
     if 'username' not in session:
         flash("Please login", 'danger')
         return redirect(url_for('login'))
 
     if request.method == "GET":
-        rooms = Room.query.all()
-        print('####################')
-        print(rooms)
-        print('#####################')
-        # return jsonify({'rooms': ROOMS})
-        return jsonify(rooms=[i.serialize for i in catalogs])
+        return jsonify({'rooms': ROOMS})
     else:
         room = request.form['room'].lower()
-        room_object = Room.query.filter_by(name=room).first()
-        print('/////////////////')
-        print(room_object)
-        print('/////////////////')
-        if not room_object:
-            room = Room(name=room)
-            db.session.add(room)
-            db.session.commit()
+
+        if room not in ROOMS:
+            ROOMS.append(room)
+            mesage[room] = []
 
     return jsonify({'success': 'Room created'})
 
+@app.route('/get-messages', methods=['POST'])
+def get_messages():
+    if 'username' not in session:
+        flash("Please login", 'danger')
+        return redirect(url_for('login'))
 
-@app.route('/logout', methods=['GET'])
+    text = []
+    room = request.form['room'].lower()
+
+    if room in mesage:
+        return jsonify({'messages': mesage[room]})
+
+    return jsonify({'messages': []})
+
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method=='POST':
+        session['username']=request.form['username']
+        return redirect(url_for('index'))
+    return render_template('login.html')
+
+
+@app.route('/logout')
 def logout():
-    logout_user()
+    session.pop('username',None)
     flash("You have logged out successfuly", "success")
     return redirect(url_for('login'))
+
 
 # server-side event handler to recivie/send messages
 @socketio.on('message')
 def message(data):
-    # print(f"\n\n{data}\n\n")
+    x = data
+    x['time_stamp'] = strftime('%b-%d %I:%M%p', localtime())
+    if data['room'] in mesage:
+        if len(mesage[data['room'].lower()]) < 5:
+            mesage[data['room'].lower()].append(x)
+        else:
+            mesage[data['room'].lower()].pop(0)
+            mesage[data['room'].lower()].append(x)
+
     send({'msg': data['msg'], 'username': data['username'],
-          'time_stamp': strftime('%b-%d %I:%M%p', localtime())}, room=data['room'])
+          'time_stamp': strftime('%b-%d %I:%M%p', localtime())}, room=data['room'].lower())
 
 
 # server-side event handler to join the room
 @socketio.on('join')
 def join(data):
-    join_room(data['room'])
-    send({"msg": data['username'] + " has joined the " + data['room'] + " room."}, room=data['room'])
+    if ROOMS:
+        join_room(data['room'])
+        send({"msg": data['username'] + " has joined the " + data['room'] + " room."}, room=data['room'])
 
 
 # server-side event handler to leave the room
@@ -154,5 +144,8 @@ def create(data):
     join_room(room)
     emit("creation", {"room": room}, broadcast=True)
 
+
 if __name__ == '__main__':
-    socketio.run(app)
+    port = int(os.environ.get('PORT', 5000))
+    # app.run(host='0.0.0.0', port=port, debug=True)
+    socketio.run(app, host='0.0.0.0', port=port)
--- static/scripts/socketio.js
+++ static/scripts/socketio.js
@@ -2,30 +2,46 @@ document.addEventListener("DOMContentLoaded", () => {
     // Connect to websocket
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
+    if (!localStorage.getItem('lastRoom')) {
+        localStorage.setItem('lastRoom', '')
+    }
+
     // make lounge is a default room
     //  and add the user to it
-    let room = 'Lounge';
-    joinRoom("Lounge")
+    let room = localStorage.getItem('lastRoom');
+    console.log(room);
+    if (room !== '') {
+        document.querySelector('#send_message').disabled = false;
+        // document.querySelector("#input-area").style.display = "block";
+        joinRoom(room)
+    } else {
+        console.log(room);
+        alert("Please create a room to start chatting")
+        document.querySelector('#send_message').display = true;
+        // document.querySelector("#input-area").style.display = "none";
+    }
 
 
-    // recieve message from the server
-    // Create a pragraph contains username, data and time
-    socket.on('message', data => {
+    function displayMessage(data) {
         const p = document.createElement('p');
         const span_username = document.createElement('span');
+        const span_data = document.createElement('span');
         const span_timestamp = document.createElement('span');
         const br = document.createElement('br');
 
         // If user write message in one of the room
         if(data.username){
             p.setAttribute("class", "my-msg");
+            p.onclick = displayFile;
             span_username.setAttribute("class", "my-username");
             span_timestamp.setAttribute("class", "timestamp");
+            span_timestamp.setAttribute("class", "data");
 
             span_username.innerHTML = data.username;
             span_timestamp.innerHTML = data.time_stamp;
+            span_data.innerHTML = data.msg;
             p.innerHTML = span_username.outerHTML + br.outerHTML +
-                          data.msg + br.outerHTML + span_timestamp.outerHTML;
+                          span_data.outerHTML + br.outerHTML + span_timestamp.outerHTML;
             document.querySelector('#display-message-section').append(p);
         }
         // If user join/leave the room
@@ -33,7 +49,29 @@ document.addEventListener("DOMContentLoaded", () => {
         else {
             printSysMsg(data.msg);
         }
+    }
 
+    function displayFile(event) {
+        event.preventDefault();
+        const file_name = this.children[2].textContent;
+
+        $.ajax({
+            type: 'get',
+            url: '/uploads/' + file_name ,
+            data: {
+
+            },
+            success: function(response) {
+                window.location.href = '/uploads/' + file_name
+            }
+        });
+
+    }
+
+    // recieve message from the server
+    // Create a pragraph contains username, data and time
+    socket.on('message', data => {
+        displayMessage(data);
     });
 
 
@@ -47,8 +85,41 @@ document.addEventListener("DOMContentLoaded", () => {
 
     }
 
+        /* attach a submit handler to the form */
+    $("#send_file_form").submit(function(event) {
+        // alert("HHHH")
+
+      /* stop form from submitting normally */
+      event.preventDefault();
+      var form_data = new FormData($('#send_file_form')[0]);
+      console.log(form_data);
+      // form_data.append('file', $('#send_file_form').prop('files')[0]);
+
+      /* get the action attribute from the <form action=""> element */
+      var form = $(this);
+      // var url = form.attr('action');
+
+      $.ajax({
+             type: "POST",
+             url: '/upload-file',
+             data: form_data, // serializes the form's elements.
+             contentType: false,
+             cache: false,
+             processData: false,
+             success: function(data)
+             {
+                 console.log(['filename']); // show response from the php script.
+                 socket.send({'msg': data['filename'],
+                                  'username': username, 'room': room
+                              });
+             }
+           });
+
+    });
+
     // Room selection
     const rooms = document.querySelectorAll('.select-room');
+    console.log(rooms);
     rooms.forEach((p) => {
         p.onclick = () => {
             let newRoom = p.innerHTML;
@@ -66,26 +137,37 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Leave room
     function leaveRoom(room) {
+        var room = room.toLowerCase();
         // use emit, beacuse its custom event
         socket.emit('leave', {'username': username, 'room': room});
     }
 
     // Join room
     function joinRoom(room) {
+        var room = room.toLowerCase();
         // use emit, beacuse its custom event
         socket.emit('join', {'username': username, 'room': room});
+        localStorage.setItem('lastRoom', room)
         // clear message area
         document.querySelector("#display-message-section").innerHTML = '';
         // Autofocus on text box
         document.querySelector("#user_message").focus();
-    }
-
-    // Print system messages
-    function printSysMsg(msg) {
-        const p = document.createElement('p');
-        p.innerHTML = msg;
-        document.querySelector("#display-message-section").append(p)
-        document.querySelector("#user_message").focus();
+        $.ajax({
+            type: 'post',
+            url: '/get-messages',
+            data: {
+                room: room
+            },
+            success: function(response) {
+                // alert("Room")
+                // displayMessage(data);
+                console.log(response);
+                for (var i = 0; i < response['messages'].length; i++) {
+                    console.log(response['messages'][i])
+                    displayMessage(response['messages'][i])
+                }
+            }
+        });
     }
 
     // When connected, check rom creation
@@ -93,9 +175,8 @@ document.addEventListener("DOMContentLoaded", () => {
           document.querySelector('#create-room').onclick = () => {
               const room = prompt("Please enter the name of the room");
               if (room != null){
-
                 socket.emit('create room', {'room': room});
-                console.log(room);
+
                 $.ajax({
                     type: 'POST',
                     url: '/get-rooms',
@@ -111,32 +192,41 @@ document.addEventListener("DOMContentLoaded", () => {
           };
       });
 
-    // Create a room in HTML
-    socket.on('creation', data => {
-        var rooms = document.querySelector('#rooms')
-        var p = document.createElement('p')
-        p.textContent = data.room
-        p.className = 'select-room'
-        console.log(p);
-        rooms.appendChild(p)
-        console.log(rooms);
-        const cx = document.querySelectorAll('.select-room');
-        console.log(cx);
-        cx.forEach((p) => {
-            p.onclick = () => {
-                let newRoom = p.innerHTML;
-                if (newRoom == room){
-                    msg = `You are already in ${room} room.`;
-                    printSysMsg(msg);
-                } else {
-                    leaveRoom(room);
-                    joinRoom(newRoom);
-                    room = newRoom;
-                }
+      // Create a room in HTML
+      socket.on('creation', data => {
+          var rooms = document.querySelector('#rooms')
+          var p = document.createElement('p')
+          p.textContent = data.room
+          p.className = 'select-room'
+          console.log(p);
+          rooms.appendChild(p)
+          console.log(rooms);
+          const cx = document.querySelectorAll('.select-room');
+          console.log(cx);
+          cx.forEach((p) => {
+              p.onclick = () => {
+                  let newRoom = p.innerHTML;
+                  if (newRoom == room){
+                      msg = `You are already in ${room} room.`;
+                      printSysMsg(msg);
+                  } else {
+                      leaveRoom(room);
+                      joinRoom(newRoom);
+                      room = newRoom;
+                  }
 
-            }
-        });
-    });
+              }
+          });
+      });
+
+
+    // Print system messages
+    function printSysMsg(msg) {
+        const p = document.createElement('p');
+        p.innerHTML = msg;
+        document.querySelector("#display-message-section").append(p)
+        document.querySelector("#user_message").focus();
+    }
 
 
 
